4bc924673ae3fd1853ce16ab4a5818825000e84c
Peter <pwgraham91@gmail.com>
2014-10-15 10:15:52 -0700

    init tests

--- playing_cards/tests.py
+++ playing_cards/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+class BasicMathTestCase(TestCase):
+    def test_math(self):
+        a = 1
+        b = 1
+        self.assertEqual(a+b, 2)
+
+    def test_failing_case(self):
+        a = 1
+        b = 1
+        self.assertEqual(a+b, 1)
